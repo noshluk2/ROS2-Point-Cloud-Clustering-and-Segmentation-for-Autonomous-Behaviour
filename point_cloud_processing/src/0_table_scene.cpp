b13a76e7bced95919a53396b0cc120e71cd2d3b0
noshluk2 <noshluk2@gmail.com>
2023-03-16 12:31:17 +0500

    Create turtlebot3 World 3D point Cloud map using Depth Camera #4

--- point_cloud_processing/src/0_table_scene.cpp
+++ point_cloud_processing/src/0_table_scene.cpp
@@ -8,7 +8,7 @@ int main(){
     pcl::PCLPointCloud2::Ptr cloud (new pcl::PCLPointCloud2());
     pcl::PCDReader cloud_reader;
 
-    std::string path = "/home/luqman/ros2_ws/src/ROS2-Point-Cloud-Clustering-and-Segmentation-for-Autonomous-Behaviour/point_cloud_processing/point_clouds/table_scene.pcd";
+    std::string path = "/home/luqman/ros2_ws/src/ROS2-Point-Cloud-Clustering-and-Segmentation-for-Autonomous-Behaviour/point_cloud_processing/point_clouds/tb3_world.pcd";
     cloud_reader.read(path,*cloud);
 
     std::cout<<"Number of point "<< cloud->width * cloud->height << std::endl;
