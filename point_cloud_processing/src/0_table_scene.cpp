3f9309107cb8a1984bbce4ffbb6702264569f35c
noshluk2 <noshluk2@gmail.com>
2023-03-16 08:05:40 +0500

    #1 loaded table_scene.pcd in our ros2 package

---
+++ point_cloud_processing/src/0_table_scene.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+
+
+int main(){
+    pcl::PCLPointCloud2::Ptr cloud (new pcl::PCLPointCloud2());
+    pcl::PCDReader cloud_reader;
+
+    std::string path = "/home/luqman/ros2_ws/src/ROS2-Point-Cloud-Clustering-and-Segmentation-for-Autonomous-Behaviour/point_cloud_processing/point_clouds/table_scene.pcd";
+    cloud_reader.read(path,*cloud);
+
+    std::cout<<"Number of point "<< cloud->width * cloud->height << std::endl;
+
+    return 0;
+}
